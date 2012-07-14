ff513197147917396be7f3cd085a3a11b7e605f6
Blossom Woo <bwoo@jawbone.com>
2012-07-14 12:57:50 -0700

    Initial project creation.

---
+++ PhotoBomber/PhotoBomber/PBViewController.h
@@ -0,0 +1,13 @@
+//
+//  PBViewController.h
+//  PhotoBomber
+//
+//  Created by Blossom Woo on 7/14/12.
+//  Copyright (c) 2012 Jawbone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBViewController : UIViewController
+
+@end
---
+++ PhotoBomber/PhotoBomber/PBViewController.m
@@ -0,0 +1,38 @@
+//
+//  PBViewController.m
+//  PhotoBomber
+//
+//  Created by Blossom Woo on 7/14/12.
+//  Copyright (c) 2012 Jawbone. All rights reserved.
+//
+
+#import "PBViewController.h"
+
+@interface PBViewController ()
+
+@end
+
+@implementation PBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end

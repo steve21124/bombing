93b229d99052dfa759b0aeee9557627fb6110883
Blossom Woo <bwoo@jawbone.com>
2012-07-14 12:57:33 -0700

    Initial Commit

---
+++ PhotoBomber/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PhotoBomber
+//
+//  Created by Blossom Woo on 7/14/12.
+//  Copyright (c) 2012 Jawbone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
